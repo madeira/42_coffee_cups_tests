9c3aa5995a99be9c7c94e39c79351b9d03651b82
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-11 12:43:20 +0200

    add models fields

--- mysite/contact/models.py
+++ mysite/contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Persone (models.Model):
+    first_name=models.CharField()
+    last_name=models.CharField()
+    date=models.DateField()
+    bio=models.TextField()
+    mail=models.EmailField()
+
+    def __unicode__(self):
+        return self.last_name
