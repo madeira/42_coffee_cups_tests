e172dbd7ad4378fe2833c930870d0ac902ff9c4e
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-11 15:00:29 +0200

    add base tests

--- mysite/contact/tests.py
+++ mysite/contact/tests.py
@@ -1,23 +1,22 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
+from django.test.client import Client
 from django.test import TestCase
+from mysite.contact.models import Persone
+import unittest
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class ContactTest(TestCase):
+    fixtures=['initial_data.json']
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+    def setUp(self):
+       contact=Persone.objects.all()
+       self.count=contact.count()
+       contact2=Persone.objects.filter(last_name="Ganziy")
+       self.assertEqual(self.count, contact2.count())
+ 
+    def test_base(self):
+        self.contact=Persone.objects.create(first_name="Ivanov", last_name="Ivan", mail="ivanov@gmail", date="1945-01-01")
+        contact_count2=Persone.objects.all()
+        self.assertNotEqual(self.count, contact_count2.count())
+        self.contact.delete()
+        contact_count2=Persone.objects.all()
+        self.assertEqual(self.count, contact_count2.count())
+ 
