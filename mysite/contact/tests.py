b35841ef2451522d3f25a789f5d4c611bb33bc55
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-14 21:41:23 +0200

    added test

--- mysite/contact/tests.py
+++ mysite/contact/tests.py
@@ -8,10 +8,10 @@ class ContactTest(TestCase):
     fixtures=['initial_data.json']
 
     def setUp(self):
-       contact=Persone.objects.all()
-       self.count=contact.count()
-       contact2=Persone.objects.filter(last_name="Ganziy")
-       self.assertEqual(self.count, contact2.count())
+        contact=Persone.objects.all()
+        self.count=contact.count()
+        contact2=Persone.objects.filter(last_name="Ganziy")
+        self.assertEqual(self.count, contact2.count())
  
     def test_base(self):
         self.contact=Persone.objects.create(first_name="Ivanov", last_name="Ivan", mail="ivanov@gmail", date="1945-01-01")
