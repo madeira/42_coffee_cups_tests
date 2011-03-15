dd073e7a93d732f1bdf38772a4d260676994ce76
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-15 22:55:21 +0200

    added virtualenv

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 project=mysite
+env=env/bin/activate
 
 test:
-	cd $(project) && python manage.py test
+	. $(env) && cd $(project) && python manage.py test
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/madeira/projects/42_test/42_coffee_cups_tests/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
