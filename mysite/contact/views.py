a96093fd255de0d8c0faf0f417aeb0a44c4fb69e
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-17 10:14:20 +0200

    modify test, veiws, initial_data, added template for errors

--- mysite/contact/views.py
+++ mysite/contact/views.py
@@ -3,5 +3,12 @@ from mysite.contact.models import Person
 
 
 def show_person(request):
-    person=Person.objects.get(last_name='Ganziy')
-    return render_to_response('base.html', {'person': person})
+    person=Person.objects.filter(last_name="Ganziy")
+    if person.count() == 1:
+        person=Person.objects.get(last_name="Ganziy")
+        return render_to_response('base.html', {'person': person})
+    elif person.count() >= 1:
+        count=person.count()
+        return render_to_response('person_error.html', {'count': count})
+    else:
+        return render_to_response('person_error_none.html', )
