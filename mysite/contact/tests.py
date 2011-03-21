8dd701f508832f56ce93a2699fe44db45b5101cf
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-21 12:12:41 +0200

    modify test

--- mysite/contact/tests.py
+++ mysite/contact/tests.py
@@ -9,20 +9,27 @@ class ContactTest(TestCase):
         response = self.client.get('/')
         self.failUnlessEqual(response.status_code, 200)
         contact=Person.objects.get(last_name="Ganziy")
-        self.assertContains(response, contact.first_name,
-                            count=1, status_code=200, msg_prefix='')
-        self.assertContains(response, contact.last_name,
-                            count=1, status_code=200, msg_prefix='')
-        self.assertContains(response, 'Sept. 24, 1986',
-                            count=1, status_code=200, msg_prefix='')
-        self.assertContains(response, contact.bio,
-                            count=1, status_code=200, msg_prefix='')
-        self.assertContains(response, contact.mail,
-                            count=1, status_code=200, msg_prefix='')
-        self.assertContains(response, contact.jabber,
-                            count=2, status_code=200, msg_prefix='')
-        self.assertContains(response, contact.skype,
-                            count=1, status_code=200, msg_prefix='')
-        self.assertContains(response, contact.other,
-                            count=1, status_code=200, msg_prefix='')
-        self.assertTemplateUsed(response, 'base.html', msg_prefix='')
+        self.assertContains(response, contact.first_name)
+        self.assertContains(response, contact.last_name)
+        self.assertContains(response, 'Sept. 24, 1986')
+        self.assertContains(response, contact.bio)
+        self.assertContains(response, contact.mail)
+        self.assertContains(response, contact.jabber)
+        self.assertContains(response, contact.skype)
+        self.assertContains(response, contact.other)
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_error_multipleobjectsreturned(self):
+        self.contact=Person.objects.create(last_name="Ganziy", date="1986-09-24")
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertContains(response, 'returned more than one Person')
+        self.assertTemplateUsed(response, 'base.html')
+        self.contact.delete()
+
+    def test_error_doesnotexist(self):
+        Person.objects.all().delete()
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertContains(response, 'Person matching query does not exist')
+        self.assertTemplateUsed(response, 'base.html')
