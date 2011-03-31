307559c9d2d381887b769fe397360b3a39029f43
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-03-31 15:14:44 +0300

    added test

--- mysite/utils/tests.py
+++ mysite/utils/tests.py
@@ -1,6 +1,8 @@
 from django.core.management import call_command
 from django.test import TestCase
 from django.db.models import get_models
+from mysite.contact.models import Person
+from mysite.utils.models import Signal
 import cStringIO
 
 
@@ -16,3 +18,18 @@ class BashTest(TestCase):
             buf2.write(info)
 
         self.assertTrue(buf2.getvalue() in buf1.getvalue())
+
+
+class SignalTest(TestCase):
+
+    def test_signal(self):
+        contact = Person.objects.create(first_name="Up", last_name="Vladimir",
+                                        date="1986-09-24", bio="up",
+                                        mail="up@up.ru", jabber="up",
+                                        skype="up", other="up")
+        signal_action = Signal.objects.order_by('-id')
+        self.assertEqual(signal_action[0].action, 'create')
+        contact.save()
+        self.assertEqual(signal_action[0].action, 'edit')
+        contact.delete()
+        self.assertEqual(signal_action[0].action, 'delete')
