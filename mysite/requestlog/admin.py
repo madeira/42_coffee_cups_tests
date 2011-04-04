6726735537a2eed77618b549e799d8cc816d4428
Ganziy Vladimir <ganziy.vova@gmail.com>
2011-04-04 13:27:36 +0300

    modify status

--- mysite/requestlog/admin.py
+++ mysite/requestlog/admin.py
@@ -2,18 +2,19 @@ from django.contrib import admin
 from mysite.requestlog.models import RequestLog
 
 
-def priority_status(modeladmin, request, queryset):
-    for count in queryset:
-        if count.status == '1':
-            queryset.filter(status='1').update(status='0')
-        else:
-            queryset.filter(status='0').update(status='1')
-priority_status.short_description = "Chenge status"
+def priority_increase(modeladmin, request, queryset):
+    queryset.update(status='1')
+priority_increase.short_description = "Increase status"
+
+
+def priority_lower(modeladmin, request, queryset):
+    queryset.update(status='0')
+priority_lower.short_description = "Lower status"
 
 
 class RequestLogAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_ip', 'status']
-    actions = [priority_status]
+    actions = [priority_increase, priority_lower]
     list_filter = ['status']
 
 admin.site.register(RequestLog, RequestLogAdmin)
